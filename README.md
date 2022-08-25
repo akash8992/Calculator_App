7b9fb8a0d1db43b8fbca8b058596b3bb0faefb7c
akash <akash8992@gmail.com>
2022-08-25 21:32:35 +0530

    bug fix

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Calculator_App
+# Flutter:Calculator
+Simple Calculator app with Flutter.
+
+NOTE: Only potrait mode.
+
+# Packages Used
+1-math_expressions v2.3.1
+
+2-get v4.5.6
+
+# Videos
+https://user-images.githubusercontent.com/98664397/184368330-56efde7d-0ffa-4dc4-a3f5-17ce91468764.mp4
+
+
+
+
---
+++ lib/data/constant.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+//colors
+
+Color white = Colors.white;
+Color black = Colors.black;
+Color blueGrey700 = Color.fromARGB(255, 255, 135, 37);
+Color blueGrey900 = Color.fromARGB(255, 3, 5, 6);
+Color green = Color.fromARGB(255, 88, 246, 177);
+Color orange = Color.fromARGB(255, 255, 148, 116);
